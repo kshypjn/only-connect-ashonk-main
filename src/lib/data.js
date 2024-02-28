a68cda1abdf3899e5bf3416e5d62505ad891c7df
Kashyap J <kashyapj@wholesomeAir.local>
2024-02-28 22:50:42 +0530

    parse

---
+++ src/lib/data.js
@@ -0,0 +1,74 @@
+export const CONNECTION_GAMES = [
+  [
+    
+    {
+      category: "Equilibrium 2.0 Sponsors",
+      words: ["Canara Bank","Too Yumm","BMW Infinity Cars","Campus Fund"],
+      difficulty:4,
+    },
+    {
+      category: "Companies with Classrooms named after them",
+      words: ["Genpact", "HDFC Bank", "Star", "Infoedge"],
+      difficulty: 3,
+    },
+    {
+      category: "____ Ghosh",
+      words: ["Munmun","Pritam","Sourav","Simantini"],
+      difficulty:1,
+    },
+    {
+      category: "_____ Banerjee",
+      words: ["Subhasish","Debapratim","Piyali","Arpan"],
+      difficulty:2,
+    },
+    
+  ],
+  [
+    {
+      category: "Club Newsletters",
+      words: ["LangVoyage","Nudgelet","The Triple Thread","Manan"],
+      difficulty:4,
+    },
+    
+    {
+      category: "BOB Bands",
+      words: ["Navtej Johar vs The Union of India","Pluto is my Planet","One Step Higher","Gaali No.4"],
+      difficulty:2,
+    },
+    {
+      category: "AshokaRejects.com",
+      words: ["Krea","Flame","Symbiosis","Jindal"],
+      difficulty:1,
+    },
+    {
+      category: "ABA 7.0 Teams",
+      words: ["Escobar","Backshots","Curry Chawal","Mango Bomb"],
+      difficulty:3,
+    },
+
+  ],
+  [
+    {
+      category: "Breakfast Porridge Flavour",
+      words: ["Berry Burst","Coconut","Plain","Tropical Fruit Punch"],
+      difficulty:3,
+    },
+    {
+      category: "Labs at Ashoka",
+      words: ["Mphasis","Fly","Psychology","Media"],
+      difficulty:2,
+    },
+    
+    {
+      category: "Short forms of Profs",
+      words: ["Peggy","Rudy","Simi","Suban"],
+      difficulty:1,
+    },
+    {
+      category: "APL 6.0 Teams",
+      words: ["Elephant","Magic Moments","Red Challengers","Banta Bros"],
+      difficulty:4,
+    },
+
+  ],
+];

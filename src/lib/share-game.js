2277ce15a6b41b65d5a85ee26e92169133b43be5
Kashyap J <kashyapj@wholesomeAir.local>
2024-03-01 17:14:57 +0530

    share improved

--- src/lib/share-game.js
+++ src/lib/share-game.js
@@ -19,7 +19,7 @@ export const shareStatus = (
 ) => {
   const GAME_URL = window.location.href;
   const textToShare =
-    `${GAME_TITLE} #${puzzleIndex}\n\n` +
+    `${GAME_TITLE}\n\n` +
     generateEmojiGrid(gameData, submittedGuesses, true) +
     `${includeGameLink ? "\n\n" + GAME_URL : ""}`;
 

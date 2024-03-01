54ef55759972864be8af7b13dd5483db7e9969a0
Kashyap J <kashyapj@wholesomeAir.local>
2024-03-01 15:10:58 +0530

    v.45

---
+++ src/components/modals/InfoModal/InfoModal.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { MAX_MISTAKES } from "../../../lib/constants";
+import { Info } from "lucide-react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../../ui/tabs";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../../ui/accordion";
+import BaseModal from "../BaseModal";
+
+function InfoModal() {
+  return (
+    <div></div>
+  );
+}
+
+export default InfoModal;

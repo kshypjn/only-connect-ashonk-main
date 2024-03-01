13c18b952ccb35ea9d4a4ea02ec220d267d6981d
Kashyap J <kashyapj@wholesomeAir.local>
2024-03-01 15:42:38 +0530

    info-modal trial

--- src/components/modals/InfoModal/InfoModal.js
+++ src/components/modals/InfoModal/InfoModal.js
@@ -12,7 +12,43 @@ import BaseModal from "../BaseModal";
 
 function InfoModal() {
   return (
-    <div></div>
+        <BaseModal
+          title=""
+          trigger={<Info className="mr-4" />}
+          initiallyOpen={false}
+          actionButtonText="Got It!"
+        >
+          <Tabs defaultValue="how-to-play">
+            <TabsList className="grid w-full grid-cols-2">
+              <TabsTrigger value="how-to-play">How To Play</TabsTrigger>
+              <TabsTrigger value="about-us">About Us</TabsTrigger>
+            </TabsList>
+            <TabsContent value="how-to-play">
+              {" "}
+              <Accordion type="single" collapsible className="w-full">
+                <AccordionItem value="item-1">
+                  <AccordionTrigger>What's The Goal?</AccordionTrigger>
+                  <AccordionContent>
+                    Find groups of items or names that share something in common.
+                  </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-2">
+                  <AccordionTrigger>How Do I Play?</AccordionTrigger>
+                  <AccordionContent>
+                    Select the items and tap 'Submit' to check if your guess matches
+                    one of the answer categories.
+                  </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-3">
+                  <AccordionTrigger>How Many Tries Do I Get?</AccordionTrigger>
+                  <AccordionContent>
+                    {`You can make ${MAX_MISTAKES} mistakes before the game ends.`}
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            </TabsContent>
+          </Tabs>
+        </BaseModal>
   );
 }
 

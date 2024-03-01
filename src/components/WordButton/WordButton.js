88ebdcb29ed8e0968df3d772a55f9b075c544d21
Kashyap J <kashyapj@wholesomeAir.local>
2024-03-01 19:33:04 +0530

    size2.0

--- src/components/WordButton/WordButton.js
+++ src/components/WordButton/WordButton.js
@@ -40,7 +40,7 @@ function WordButton({ word, fullCandidateSize }) {
   //const fontSizeByWordLength = 9characters works with 0.6rem
 
   function getFontSize(word) {
-    const baseLength = 10;
+    const baseLength = 7;
     const wordLength = word.length;
     let fontSize = 1;
     if (wordLength > baseLength) {

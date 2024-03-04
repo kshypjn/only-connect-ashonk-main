146121984e83ce92439b0178b23d958b008c780b
Kashyap J <kashyapj@wholesomeAir.local>
2024-03-04 14:30:30 +0530

    game

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,8 +15,8 @@ function App() {
           <Header />
           <Game />
         </div>
+        <Analytics />
       </GameStatusProvider>
-      <Analytics />
     </PuzzleDataProvider>
     
     </>

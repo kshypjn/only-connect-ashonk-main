a4513167413d0fcf00268073e65bd62a402dd4d0
Kashyap J <kashyapj@wholesomeAir.local>
2024-09-01 12:20:55 +0530

    gameS

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,308 +1,308 @@
 export const CONNECTION_GAMES = [
 
-  // [
-  //   {
-  //     category: "Club Newsletters",
-  //     words: ["LangVoyage","Nudgelet","The Triple Thread","Manan"],
-  //     difficulty:4,
-  //   },
+  [
+    {
+      category: "Club Newsletters",
+      words: ["LangVoyage","Nudgelet","The Triple Thread","Manan"],
+      difficulty:4,
+    },
     
-  //   {
-  //     category: "BOB Bands",
-  //     words: ["Navtej Johar vs The Union of India","Pluto is my Planet","One Step Higher","Gaali No.4"],
-  //     difficulty:2,
-  //   },
-  //   {
-  //     category: "AshokaRejects.com",
-  //     words: ["Krea","Flame","Symbiosis","Jindal"],
-  //     difficulty:1,
-  //   },
-  //   {
-  //     category: "ABA 7.0 Teams",
-  //     words: ["Escobar","Backshots","Curry Chawal","Mango Bomb"],
-  //     difficulty:3,
-  //   },
+    {
+      category: "BOB Bands",
+      words: ["Navtej Johar vs The Union of India","Pluto is my Planet","One Step Higher","Gaali No.4"],
+      difficulty:2,
+    },
+    {
+      category: "AshokaRejects.com",
+      words: ["Krea","Flame","Symbiosis","Jindal"],
+      difficulty:1,
+    },
+    {
+      category: "ABA 7.0 Teams",
+      words: ["Escobar","Backshots","Curry Chawal","Mango Bomb"],
+      difficulty:3,
+    },
 
-  // ],
-  // [
-  //   {
-  //     category: "Breakfast Porridge Flavour",
-  //     words: ["Berry Burst","Coconut","Plain","Tropical Fruit Punch"],
-  //     difficulty:3,
-  //   },
-  //   {
-  //     category: "Labs at Ashoka",
-  //     words: ["Mphasis","Fly","Psychology","Media"],
-  //     difficulty:2,
-  //   },
+  ],
+  [
+    {
+      category: "Breakfast Porridge Flavour",
+      words: ["Berry Burst","Coconut","Plain","Tropical Fruit Punch"],
+      difficulty:3,
+    },
+    {
+      category: "Labs at Ashoka",
+      words: ["Mphasis","Fly","Psychology","Media"],
+      difficulty:2,
+    },
     
-  //   {
-  //     category: "Short forms of Profs",
-  //     words: ["Peggy","Rudy","Simi","Suban"],
-  //     difficulty:1,
-  //   },
-  //   {
-  //     category: "APL 6.0 Teams",
-  //     words: ["Elephant","Magic Moments","KFC","Banta Bros"],
-  //     difficulty:4,
-  //   },
+    {
+      category: "Short forms of Profs",
+      words: ["Peggy","Rudy","Simi","Suban"],
+      difficulty:1,
+    },
+    {
+      category: "APL 6.0 Teams",
+      words: ["Elephant","Magic Moments","KFC","Banta Bros"],
+      difficulty:4,
+    },
 
-  // ],
-  // [
+  ],
+  [
     
-  //   {
-  //     category: "Equilibrium 2.0 Sponsors",
-  //     words: ["Canara Bank","Too Yumm","IDP","Campus Fund"],
-  //     difficulty:4,
-  //   },
-  //   {
-  //     category: "Companies with Campus Spots named after them",
-  //     words: ["Genpact", "HDFC Bank", "Star", "Infoedge"],
-  //     difficulty: 3,
-  //   },
-  //   {
-  //     category: "____ Ghosh",
-  //     words: ["Munmun","Pritam","Sourav","Simantini"],
-  //     difficulty:1,
-  //   },
-  //   {
-  //     category: "_____ Banerjee",
-  //     words: ["Subhasish","Debapratim","Piyali","Arpan"],
-  //     difficulty:2,
-  //   },
+    {
+      category: "Equilibrium 2.0 Sponsors",
+      words: ["Canara Bank","Too Yumm","IDP","Campus Fund"],
+      difficulty:4,
+    },
+    {
+      category: "Companies with Campus Spots named after them",
+      words: ["Genpact", "HDFC Bank", "Star", "Infoedge"],
+      difficulty: 3,
+    },
+    {
+      category: "____ Ghosh",
+      words: ["Munmun","Pritam","Sourav","Simantini"],
+      difficulty:1,
+    },
+    {
+      category: "_____ Banerjee",
+      words: ["Subhasish","Debapratim","Piyali","Arpan"],
+      difficulty:2,
+    },
     
-  // ],
-  // [
-  //   {
-  //     category: "Nescafe Maggi Flavours",
-  //     words: ["Original","Biryani","Peri Peri","Special"],
-  //     difficulty:2,
-  //   },
-  //   {
-  //     category: "Rooms at Ashoka",
-  //     words: ["Reading","Music","Mail","Yoga"],
-  //     difficulty:3,
-  //   },
+  ],
+  [
+    {
+      category: "Nescafe Maggi Flavours",
+      words: ["Original","Biryani","Peri Peri","Special"],
+      difficulty:2,
+    },
+    {
+      category: "Rooms at Ashoka",
+      words: ["Reading","Music","Mail","Yoga"],
+      difficulty:3,
+    },
     
-  //   {
-  //     category: "Ultimate Frisbee Terms",
-  //     words: ["Stack","Brick","Dump","Hammer"],
-  //     difficulty:4,
-  //   },
-  //   {
-  //     category: "Sports Block Ground Floor",
-  //     words: ["Infirmary","Salon","Dance Studio","Pool Table"],
-  //     difficulty:1,
-  //   },
+    {
+      category: "Ultimate Frisbee Terms",
+      words: ["Stack","Brick","Dump","Hammer"],
+      difficulty:4,
+    },
+    {
+      category: "Sports Block Ground Floor",
+      words: ["Infirmary","Salon","Dance Studio","Pool Table"],
+      difficulty:1,
+    },
 
-  // ],
-  // [
-  //   {
-  //     category: "Ashoka Chapters of Organisations",
-  //     words: ["Enactus","IEEE","HerCampus","SOBAC"],
-  //     difficulty:4,
-  //   },
-  //   {
-  //     category: "_____ Collective",
-  //     words: ["Art & Design","Feminist","Northeast","Democracy"],
-  //     difficulty:3,
-  //   },
+  ],
+  [
+    {
+      category: "Ashoka Chapters of Organisations",
+      words: ["Enactus","IEEE","HerCampus","SOBAC"],
+      difficulty:4,
+    },
+    {
+      category: "_____ Collective",
+      words: ["Art & Design","Feminist","Northeast","Democracy"],
+      difficulty:3,
+    },
     
-  //   {
-  //     category: "Performing Arts Clubs",
-  //     words: ["Vistaar","The Comic Relief","Abhinaya","The Green Room"],
-  //     difficulty:2,
-  //   },
-  //   {
-  //     category: "Departments of The Edict",
-  //     words: ["Sports","News","Art & Culture","Opinions"],
-  //     difficulty:1,
-  //   },
-  // ],
-  // [
-  //   {
-  //     category: "Bombay(the show) Characters",
-  //     words: ["Pranav Ramanathan","Gayatri Mishra","Ananya Sinha","Aaryan Dhingra"],
-  //     difficulty:4,
-  //   },
+    {
+      category: "Performing Arts Clubs",
+      words: ["Vistaar","The Comic Relief","Abhinaya","The Green Room"],
+      difficulty:2,
+    },
+    {
+      category: "Departments of The Edict",
+      words: ["Sports","News","Art & Culture","Opinions"],
+      difficulty:1,
+    },
+  ],
+  [
+    {
+      category: "Bombay(the show) Characters",
+      words: ["Pranav Ramanathan","Gayatri Mishra","Ananya Sinha","Aaryan Dhingra"],
+      difficulty:4,
+    },
     
-  //   {
-  //     category: "Bald cc:Neeraja S(UG24)",
-  //     words: ["Anup Padmanabhan", "Arunava Sinha", "Abir Bazaz", "Biswajeet Banerjee"],
-  //     difficulty:3,
-  //   },
+    {
+      category: "Bald cc:Neeraja S(UG24)",
+      words: ["Anup Padmanabhan", "Arunava Sinha", "Abir Bazaz", "Biswajeet Banerjee"],
+      difficulty:3,
+    },
    
-  //   {
-  //     category: "Head of Departments",
-  //     words: ["Rajendra Bhatia","Tisha Srivastav","Justin McCarthy","Kathryn Hardy"],
-  //     difficulty:1,
-  //   },
-  //   {
-  //     category: "Prof whose partners also taught at Ashoka(exes count shush)",
-  //     words: ["Madhavi Menon","Kranti Saran","Aditi Sriram","Nayan Chanda"],
-  //     difficulty:2,
-  //   },
-  // ],
-  // [
-  //   {
-  //     category: "Statues on Campus",
-  //     words: ["Hand","Book","Rat","Gandhi"],
-  //     difficulty:2,
-  //   },
-  //   {
-  //     category: "Names of Dogs Outside Campus(cc: Pawsitive)",
-  //     words: ["Kiwi","Sutta","Brownie","Popcorn"],
-  //     difficulty:4,
-  //   },
+    {
+      category: "Head of Departments",
+      words: ["Rajendra Bhatia","Tisha Srivastav","Justin McCarthy","Kathryn Hardy"],
+      difficulty:1,
+    },
+    {
+      category: "Prof whose partners also taught at Ashoka(exes count shush)",
+      words: ["Madhavi Menon","Kranti Saran","Aditi Sriram","Nayan Chanda"],
+      difficulty:2,
+    },
+  ],
+  [
+    {
+      category: "Statues on Campus",
+      words: ["Hand","Book","Rat","Gandhi"],
+      difficulty:2,
+    },
+    {
+      category: "Names of Dogs Outside Campus(cc: Pawsitive)",
+      words: ["Kiwi","Sutta","Brownie","Popcorn"],
+      difficulty:4,
+    },
     
-  //   {
-  //     category: "Vow Spice (TKS) Outlets",
-  //     words: ["Charcoal","Go South","Alfredo's","Wok Tossed"],
-  //     difficulty:1,
-  //   },
-  //   {
-  //     category: "Mojitos at Dosai",
-  //     words: ["Bull","Mango","Watermelon","Mint"],
-  //     difficulty:3,
-  //   },
-  // ],
+    {
+      category: "Vow Spice (TKS) Outlets",
+      words: ["Charcoal","Go South","Alfredo's","Wok Tossed"],
+      difficulty:1,
+    },
+    {
+      category: "Mojitos at Dosai",
+      words: ["Bull","Mango","Watermelon","Mint"],
+      difficulty:3,
+    },
+  ],
   
   
-  // [
-  //   {
-  //     category: "Located in the Basement cc:Madhavan J(UG24)",
-  //     words: ["Tuck Shop","Laundry","Lockers","Music Room"],
-  //     difficulty:3,
-  //   },
-  //   {
-  //     category: "Places with Table Tennis",
-  //     words: ["RH1","RH3","Sports Block","RH5"],
-  //     difficulty:2,
-  //   },
+  [
+    {
+      category: "Located in the Basement cc:Madhavan J(UG24)",
+      words: ["Tuck Shop","Laundry","Lockers","Music Room"],
+      difficulty:3,
+    },
+    {
+      category: "Places with Table Tennis",
+      words: ["RH1","RH3","Sports Block","RH5"],
+      difficulty:2,
+    },
     
-  //   {
-  //     category: "Located in AC04",
-  //     words: ["ACWB","Library","VOW Cafe","Archives"],
-  //     difficulty:4,
-  //   },
-  //   {
-  //     category: "Performance Venues",
-  //     words: ["Black Box","Reddy's","Amphitheatre","Mess Lawns"],
-  //     difficulty:1,
-  //   },
-  // ],
-  // [
-  //   {
-  //     category: "______ Bhattacharya",
-  //     words: ["Alok","Neeladri","Mihir","Dipankar"],
-  //     difficulty:1,
-  //   },
-  //   {
-  //     category: "Sharmajis cc: Aditya Aiyer(UG25)",
-  //     words: ["Anisha","Ananya","Manvi","Mukul"],
-  //     difficulty:2,
-  //   },
+    {
+      category: "Located in AC04",
+      words: ["ACWB","Library","VOW Cafe","Archives"],
+      difficulty:4,
+    },
+    {
+      category: "Performance Venues",
+      words: ["Black Box","Reddy's","Amphitheatre","Mess Lawns"],
+      difficulty:1,
+    },
+  ],
+  [
+    {
+      category: "______ Bhattacharya",
+      words: ["Alok","Neeladri","Mihir","Dipankar"],
+      difficulty:1,
+    },
+    {
+      category: "Sharmajis cc: Aditya Aiyer(UG25)",
+      words: ["Anisha","Ananya","Manvi","Mukul"],
+      difficulty:2,
+    },
     
-  //   {
-  //     category: "No Major, Only Minor",
-  //     words: ["Astronomy","Visual Arts","Performing Arts","Environmental Studies"],
-  //     difficulty:3,
-  //   },
-  //   {
-  //     category: "____ + Phil (PPE,PHICS,PSYPHI) cc:Pranav A(UG2023)",
-  //     words: ["Psychology","Politcal Science","Computer Science","Economics"],
-  //     difficulty:4,
-  //   },
-  // ],
-  // [
-  //   {
-  //     category: "Located in Admin Block cc:Tejas N(UG25)",
-  //     words: ["MPH","OAA","CDO","CWC"],
-  //     difficulty:1,
-  //   },
-  //   {
-  //     category: "Former Ashokan Political Parties",
-  //     words: ["Prakrit","Liberandu","ASS","Sattva"],
-  //     difficulty:2,
-  //   },
+    {
+      category: "No Major, Only Minor",
+      words: ["Astronomy","Visual Arts","Performing Arts","Environmental Studies"],
+      difficulty:3,
+    },
+    {
+      category: "____ + Phil (PPE,PHICS,PSYPHI) cc:Pranav A(UG2023)",
+      words: ["Psychology","Politcal Science","Computer Science","Economics"],
+      difficulty:4,
+    },
+  ],
+  [
+    {
+      category: "Located in Admin Block cc:Tejas N(UG25)",
+      words: ["MPH","OAA","CDO","CWC"],
+      difficulty:1,
+    },
+    {
+      category: "Former Ashokan Political Parties",
+      words: ["Prakrit","Liberandu","ASS","Sattva"],
+      difficulty:2,
+    },
     
-  //   {
-  //     category: "Ashonk Instagram Accounts",
-  //     words: ["Odomos","Random","Rooms","Toofan"],
-  //     difficulty:3,
-  //   },
-  //   {
-  //     category: "Learning Platforms",
-  //     words: ["Classroom","Perusall","Canvas","Piazza"],
-  //     difficulty:4,
-  //   },
-  // ],
-  // [
-  //   {
-  //     category: "Mess Paneers",
-  //     words: ["Handi","Banjara","Kali Mirch","Makhana"],
-  //     difficulty:1,
-  //   },
-  //   {
-  //     category: "Mess Pasta",
-  //     words: ["Mix Sauce","Homestyle","Marinara","Salad"],
-  //     difficulty:2,
-  //   },
+    {
+      category: "Ashonk Instagram Accounts",
+      words: ["Odomos","Random","Rooms","Toofan"],
+      difficulty:3,
+    },
+    {
+      category: "Learning Platforms",
+      words: ["Classroom","Perusall","Canvas","Piazza"],
+      difficulty:4,
+    },
+  ],
+  [
+    {
+      category: "Mess Paneers",
+      words: ["Handi","Banjara","Kali Mirch","Makhana"],
+      difficulty:1,
+    },
+    {
+      category: "Mess Pasta",
+      words: ["Mix Sauce","Homestyle","Marinara","Salad"],
+      difficulty:2,
+    },
     
-  //   {
-  //     category: "Foods that inspired Outlet Names",
-  //     words: ["Pizza","Dosa","Roti","Chai"],
-  //     difficulty:3,
-  //   },
-  //   {
-  //     category: "Lovsy Flavours",
-  //     words: ["Chatpata", "Spicy", "Salty", "Timmur"],
-  //     difficulty:4,
-  //   },
-  // ],
-  // [
-  //   {
-  //     category: "Profs taught by other Ashoka Profs cc:Roshni A(UG25)",
-  //     words: ["Anirban Sen", "Anuradha Saha", "Kumarjit Saha", "Goutam Paul"],
-  //     difficulty:3,
-  //   },
-  //   {
-  //     category: "TGR Plays",
-  //     words: ["Band Darvaze","The Inheritance","I Don't Like This","Hold The Mushrooms"],
-  //     difficulty:1,
-  //   },
-  //   {
-  //     category: "HerCampus Relationship Advice Reel",
-  //     words: ["Madhavi Menon","Jonathan Gil Harris","Ben Winokur","Abir Bazaz"],
-  //     difficulty:2,
-  //   },
-  //   {
-  //     category: "Screening Spots",
-  //     words: ["02-007","RH5","MPH","TV Lounge"],
-  //     difficulty:4,
-  //   },
-  // ],
-  // [
-  //   {
-  //     category: "Vice Chancellors of Ashoka",
-  //     words: ["Somak", "Malabika", "Pratap", "Rudrangshu"],
-  //     difficulty:1,
-  //   },
-  //   {
-  //     category: "You tap your ID Card Here cc:Yashwanth (UG2023)",
-  //     words: ["Mess","RH Commons","Main Gate","Library"],
-  //     difficulty:4,
-  //   },
-  //   {
-  //     category: "RH1 Ground Floor",
-  //     words: ["Siyahi Room","Pool Table","Washing Machine","Vending Machine"],
-  //     difficulty:2,
-  //   },
-  //   {
-  //     category: "____ Studies",
-  //     words: ["China","War","Media","Environmental"],
-  //     difficulty:3,
-  //   }, ]
+    {
+      category: "Foods that inspired Outlet Names",
+      words: ["Pizza","Dosa","Roti","Chai"],
+      difficulty:3,
+    },
+    {
+      category: "Lovsy Flavours",
+      words: ["Chatpata", "Spicy", "Salty", "Timmur"],
+      difficulty:4,
+    },
+  ],
+  [
+    {
+      category: "Profs taught by other Ashoka Profs cc:Roshni A(UG25)",
+      words: ["Anirban Sen", "Anuradha Saha", "Kumarjit Saha", "Goutam Paul"],
+      difficulty:3,
+    },
+    {
+      category: "TGR Plays",
+      words: ["Band Darvaze","The Inheritance","I Don't Like This","Hold The Mushrooms"],
+      difficulty:1,
+    },
+    {
+      category: "HerCampus Relationship Advice Reel",
+      words: ["Madhavi Menon","Jonathan Gil Harris","Ben Winokur","Abir Bazaz"],
+      difficulty:2,
+    },
+    {
+      category: "Screening Spots",
+      words: ["02-007","RH5","MPH","TV Lounge"],
+      difficulty:4,
+    },
+  ],
+  [
+    {
+      category: "Vice Chancellors of Ashoka",
+      words: ["Somak", "Malabika", "Pratap", "Rudrangshu"],
+      difficulty:1,
+    },
+    {
+      category: "You tap your ID Card Here cc:Yashwanth (UG2023)",
+      words: ["Mess","RH Commons","Main Gate","Library"],
+      difficulty:4,
+    },
+    {
+      category: "RH1 Ground Floor",
+      words: ["Siyahi Room","Pool Table","Washing Machine","Vending Machine"],
+      difficulty:2,
+    },
+    {
+      category: "____ Studies",
+      words: ["China","War","Media","Environmental"],
+      difficulty:3,
+    }, ]
   [
     {
         category: "Ashoka Fests",
@@ -325,28 +325,28 @@ export const CONNECTION_GAMES = [
             difficulty:2,
         }
   ],
-  // [
-  //   {
-  //         category: "Sports Block Ground Floor",
-  //         words: ["Infirmary","Salon","Dance Studio","Pool Table"],
-  //         difficulty:1,
-  //       },
-  //       {
-  //             category: "Labs at Ashoka",
-  //             words: ["Mphasis","Fly","Psychology","Media"],
-  //             difficulty:2,
-  //           },
-  //           {
-  //                 category: "Performance Venues",
-  //                 words: ["Black Box","Reddy's","Amphitheatre","Mess Lawns"],
-  //                 difficulty:3,
-  //               },
-  //               {
-  //                 category: "Ashoka Fests",
-  //                 words: ["Agneepath","Chimera","Banjaara","Equilibrium"],
-  //                 difficulty:4,
-  //               },
+  [
+    {
+          category: "Sports Block Ground Floor",
+          words: ["Infirmary","Salon","Dance Studio","Pool Table"],
+          difficulty:1,
+        },
+        {
+              category: "Labs at Ashoka",
+              words: ["Mphasis","Fly","Psychology","Media"],
+              difficulty:2,
+            },
+            {
+                  category: "Performance Venues",
+                  words: ["Black Box","Reddy's","Amphitheatre","Mess Lawns"],
+                  difficulty:3,
+                },
+                {
+                  category: "Ashoka Fests",
+                  words: ["Agneepath","Chimera","Banjaara","Equilibrium"],
+                  difficulty:4,
+                },
 
-  // ]
+  ]
  
 ];
